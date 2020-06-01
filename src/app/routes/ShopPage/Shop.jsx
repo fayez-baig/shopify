67e148bdf4079bf6f74eddb7ae375ce66ca1a419
fayez baig <coderninja16@gmail.com>
2020-06-02 04:46:42 +0530

    added new fonts

--- src/app/routes/ShopPage/Shop.jsx
+++ src/app/routes/ShopPage/Shop.jsx
@@ -6,14 +6,12 @@ import CollectionPreview from "./CollectionPreview";
 class ShopPage extends React.Component {
   constructor(props) {
     super(props);
-
     this.state = {
       collections: ShopData,
     };
   }
   render() {
     const { collections } = this.state;
-    console.log("hello");
     return (
       <div className="shop-page">
         {collections.map(({ id, ...otherProps }) => (
